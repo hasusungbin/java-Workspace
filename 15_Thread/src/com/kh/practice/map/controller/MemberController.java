6846d5408ecf312fbcc6f8f8f06ba73030130c81
binryun <binryun0@gmail.com>
2022-12-08 15:14:02 +0900

    12 / 08
    12 / 08

---
+++ 15_Thread/src/com/kh/practice/map/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.kh.practice.map.controller;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeMap;
+
+import com.kh.practice.map.model.vo.Member;
+
+public class MemberController {
+	private HashMap<String , Member> map = new HashMap();
+	
+	public boolean joinMembership(String id, Member m) {
+		// 전달 받은 id 가 없다면 id와 m을 map에 추가 후 true 반환
+		// 있다면 false 값 반환
+		if(map.get(id) == null) {
+			map.put(id, m);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String logIn(String id, String password) {
+		String name = null;
+		if(map.get(id) != null && map.get(id).getPassword().equals(password)) {
+			name = map.get(id).getName();
+		}
+		return name;
+	}
+	
+	public boolean changePassword(String id, String oldPw, String newPw) {
+		if(map.get(id) != null && map.get(id).getPassword().equals(oldPw)) {
+			map.get(id).setPassword(newPw);
+			return true;
+		}
+		return false;
+	}
+	
+	public void changeName(String id, String newName) {
+		map.get(id).setName(newName);
+	}
+	
+	public TreeMap sameName(String name) {
+		Set<String> set = map.keySet();
+		TreeMap<String, String> tm = new TreeMap();
+		for(String key : set) {
+			if(map.get(key).getName().equals(name)) {
+				tm.put(key, name);
+			}
+		}
+		return tm;
+	}
+}
